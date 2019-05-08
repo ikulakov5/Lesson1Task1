ebc2ddff95e0d4aa193c97856e447143a8ba1417
iKulakov <codukulakov@gmail.com>
2019-05-08 23:58:25 +0300

    ready

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,21 @@
-alert("Привет");
+const price = [200, 550, 4000, 23, 58, 5000, 485, 711];
+let finalPrice = 0;
+let finalBonus = 0;
+
+const calcBonus = (price) => {
+
+  for (let value of price) {
+    finalPrice += value;
+  }
+  if(finalPrice > 10000) {
+    finalBonus = (finalPrice * 0.05);
+    finalBonus = Math.round(finalBonus);
+  }
+  else {
+    finalBonus = 0;
+  }
+  return finalBonus;
+
+}
+
+console.log("Ваш бонус:", calcBonus(price));
